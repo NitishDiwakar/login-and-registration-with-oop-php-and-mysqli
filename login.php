3a688a70723506da59140f1ce3ee33d18f53ba3d
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2017-06-25 15:21:38 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,12 +8,14 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<div class="container">
-	<div class="row" style="margin-top: 10%;">
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
+	<div style="margin-top: 4%;"></div>
+	<div class="row">
+	<div class="col-sm-4"></div>
+	<div class="col-sm-4 well">
 	<h2 class="text-center">Login Here</h2>
 		<form action="include/login.php" method="post" name="login">
 		<div class="form-group">
@@ -36,7 +38,7 @@
 		}
 	?>
 	</div>
-	<div class="col-md-3"></div>
+	<div class="col-sm-4"></div>
 	</div> <!-- End row -->
 	</div> <!-- End container -->
 </body>
