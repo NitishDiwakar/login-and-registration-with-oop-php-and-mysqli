19d92ed4a7404bd843a116f369e3a5df63940024
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2017-06-25 15:20:02 +0530

    Update home.php

--- home.php
+++ home.php
@@ -21,6 +21,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	
